d0f6f6b37015a975896a3035648086e95bda51eb
Gabriel Luiz Kunz <gabriel.kunz@grupodass.com.br>
2026-04-27 19:42:01 -0300

    nova rota de recuperacao de senha

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -3,4 +3,5 @@ const controller = require('../controllers/auth')
 module.exports = (app) => {
   app.post('/auth/register', controller.register)
   app.post('/auth/login', controller.login)
+  app.post('/auth/recovery', (req, res) => res.status(200).send())
 }
